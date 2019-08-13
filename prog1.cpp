c8eedb7749402d031dc2e33bf61295905f3b1117
Mounisha3232 <54067768+Mounisha3232@users.noreply.github.com>
2019-08-13 18:47:58 +0530

    Create prog1.cpp

---
+++ prog1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+if(n<0)
+  cout<<"Negative";
+else if(n>0)
+   cout<<"Positive";
+else
+   cout<<"Zero";
+ return 0;
+ }
+  
